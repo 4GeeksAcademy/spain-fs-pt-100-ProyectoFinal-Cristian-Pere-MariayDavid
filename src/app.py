6be4bfc1cf2cf38f174a9c362f66c0fdd11ecbed
DaivdVivar <davidvivarmartinez@gmail.com>
2025-06-03 08:57:26 +0000

    tarifita

--- src/app.py
+++ src/app.py
@@ -23,6 +23,12 @@ app.url_map.strict_slashes = False
 app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
 jwt = JWTManager(app)
 
+
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
+jwt = JWTManager(app)
+
+
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
